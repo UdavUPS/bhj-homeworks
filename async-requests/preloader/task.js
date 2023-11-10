a7a43db32fe9c37d1f0dcdef69b55e86170acf1e
Udav-94 <ups-fti@yandex.ru>
2023-11-10 18:38:57 +0300

    Д.з.3.1 полностью исправлен №1

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,6 +1,6 @@
 const xhr = new XMLHttpRequest();
 const loader = document.querySelector('.loader');
-const item = document.querySelector('.item');
+const items = document.getElementById('items');
 
 xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/slow-get-courses', true);
 xhr.send();
@@ -21,18 +21,23 @@ xhr.addEventListener ('readystatechange', () => {
 })
 
 function elementCreate (codeValue, valueValue, currencyValue) {
+    let rootElement = document.createElement ('div');
+    rootElement.classList.add('item');
+
     let codeElement = document.createElement ('div');
     codeElement.classList.add('item__code');
     codeElement.innerHTML = codeValue;
-    item.appendChild(codeElement);
+    rootElement.appendChild(codeElement);
 
     let valueElement = document.createElement ('div');
     valueElement.classList.add('item__value');
     valueElement.innerHTML = valueValue;
-    item.appendChild(valueElement);
+    rootElement.appendChild(valueElement);
 
     let currencyElement = document.createElement ('div');
     currencyElement.classList.add('item__currency');
     currencyElement.innerHTML = currencyValue;
-    item.appendChild(currencyElement);
+    rootElement.appendChild(currencyElement);
+
+    items.appendChild(rootElement);
 }
