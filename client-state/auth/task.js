c7751f59122953c55cf634167f0510f28270519c
Udav-94 <ups-fti@yandex.ru>
2023-12-02 16:15:01 +0300

    3.2 Задание №3 реализовано окно приветствия и попытка вывести ответ

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,6 +1,19 @@
 const form = document.getElementById('signin__form');
 const btn = document.querySelector('.btn');
 /* let FormD = new FormData(form); */
+const welcomeWindow = document.getElementById('welcome');
+const userName = document.getElementById('user_id');
+let userId;
+let userStatus;
+
+function saveStatus (userStatus, userId) {
+    if (userStatus) {
+        localStorage.setItem('user_Id', userId);
+        userName.innerHTML = userId;
+        welcomeWindow.classList.add('welcome_active');
+    }
+  }
+
 
 
 form.addEventListener('submit', (e) => {
@@ -8,10 +21,27 @@ form.addEventListener('submit', (e) => {
     const xhr = new XMLHttpRequest();
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
     let FormD = new FormData(document.forms.signin__form);
-/*     FormD.append('login', document.forms.login.value);
-    FormD.append('password', document.forms.password.value); */
     xhr.send(FormD);
+
+
+    let psrsXHR = JSON.parse(xhr.responseText);
+    saveStatus (psrsXHR.success, psrsXHR.user_id);
+
+
+
+
+    console.log(xhr);
+    console.log(xhr.responseText);
+    //saveStatus (userStatus, userId);
+
+
+    /* saveStatus (userStatus, userId);
+    //let parsXHR = JSON.parse(xhr);
+    userId = xhr.response;    ;
+    console.log(userId); */
   });
+
+
 
 
  /* btn.addEventListener('click', (e)=>{
